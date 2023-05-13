698a188e26de349f492e673b5255fbfeeb0e39d5
bogdan198731 <bogdan198731@gmail.com>
2023-05-13 14:14:56 +0300

    Proiect Licenta commit test

---
+++ proiectLicenta/cryptoMoneda.py
@@ -0,0 +1,23 @@
+
+
+class CryptoMoneda:
+    def __init__(self, nume, istoric_preturi, istoric_volum, initialize):
+        self.nume = nume
+        self.istoric_preturi = istoric_preturi
+        self.istoric_volum = istoric_volum
+        self.initialize = initialize
+
+    def update(self, pret):
+        try:
+            self.pref = float(pret)
+            self.istoric_preturi.append(self.pret)
+            self.pretOk = True
+        except:
+            self.pretOk = False
+
+
+    def displayTest(self):
+        print("crypto " + self.nume+ " are pret " + self.pret + " si istoric ")
+        for pret in self.istoric_preturi:
+
+
